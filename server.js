21c82f61af1618a113944b7582ff017396dc9f16
Abdel Jalile CHAHID <abdeljalile.chahid@gmail.com>
2019-03-01 17:04:52 +0100

    test server

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const app = require('./app');
 //Initialisation du port d'écoute
 const port = process.env.PORT || 3000;
 
+var instructionsNouveauVisiteur = function(req, res) {
+  res.writeHead(200);
+  res.end('Salut tout le monde !');
+}
+
 // création du serveur
-const server = http.Server(app);
+const server = http.Server(instructionsNouveauVisiteur);
 
 server.listen(port);
