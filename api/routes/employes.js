a1659a0ceb895bd21dfffc44a75fc584881a8641
Abdel Jalile CHAHID <abdeljalile.chahid@gmail.com>
2019-03-13 15:02:35 +0100

    const bcrypt = require("bcrypt");

--- api/routes/employes.js
+++ api/routes/employes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const Employe = require("../models/employes");
 const Entreprise = require("../models/entreprises");
